40b2b4bb80eecf2a69c151f6da299d0aa9a8501c
AbsGam <gabsaidov27@gmail.com>
2026-08-08 19:01:50 +0300

    script.py

--- script.py
+++ script.py
@@ -32,4 +32,4 @@ def main_page(current_user):
 def cow_moo(name):
     return render_template('korova.html', name=name)
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
